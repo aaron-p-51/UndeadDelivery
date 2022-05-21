83949fc6675dba2f3e8964484770c69da0158519
Aaron <petrekaj@gmail.com>
2022-05-21 01:35:39 -0700

    Upload UE4 project

---
+++ Source/UndeadDelivery/UndeadDeliveryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UndeadDeliveryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDEADDELIVERY_API AUndeadDeliveryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
